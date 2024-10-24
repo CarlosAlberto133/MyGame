d723d291570c8d36fc4a5ea8e061d958413d52ea
Carlos Alberto <carlos.neurozze@gmail.com>
2024-10-23 22:01:33 -0300

    initial project, start board

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [Header("Art stuff")]
+    [SerializeField] private Material tileMaterial;
+    [SerializeField] private float tileSize = 1.0f;
+    [SerializeField] private float yOffSet = 0.2f;
+    [SerializeField] private Vector3 boardCenter = Vector3.zero;
+
+    [Header("Prefabs & Materials")]
+    [SerializeField] private GameObject[] prefabs;
+    [SerializeField] private Material[] teamMaterials;
+
+    // LOGIC
+    private const int TILE_COUNT_X = 8;
+    private const int TILE_COUNT_Y = 8;
+    private GameObject[,] tiles;
+    private Camera currentCamera;
+    private Vector2Int currentHover;
+    private Vector3 bounds;
+
+    private void Awake()
+    {
+        GenerateAllTiles(tileSize, TILE_COUNT_X, TILE_COUNT_Y);
+
+        SpawnSingleCard(CardPieceType.King, 0);
+    }
+
+    private void Update()
+    {
+        if(!currentCamera)
+        {
+            currentCamera = Camera.main;
+            return;
+        }
+
+        RaycastHit info;
+        Ray ray = currentCamera.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out info, 100, LayerMask.GetMask("Tile", "Hover")))
+        {
+            // Get the indexes of the tile i've hit
+            Vector2Int hitPosition = LookupTileIndex(info.transform.gameObject);
+
+            // If we're hovering a tile after not hovering any tiles
+            if(currentHover == -Vector2Int.one)
+            {
+                currentHover = hitPosition;
+                tiles[hitPosition.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+            }
+
+            // If we were already hovering a tile, change the previous one
+            if(currentHover != hitPosition)
+            {
+                tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
+                currentHover = hitPosition;
+                tiles[hitPosition.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+            }
+        }
+        else
+        {
+            if(currentHover != -Vector2Int.one)
+            {
+                tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
+                currentHover = -Vector2Int.one;
+            }
+        }
+    }
+
+    // Generate the board
+    private void GenerateAllTiles(float tileSize, int tileCountX, int tileCountY)
+    {
+        yOffSet += transform.position.y;
+        bounds = new Vector3((tileCountX / 2) * tileSize, 0, (tileCountX / 2) * tileSize) + boardCenter;
+
+        tiles = new GameObject[tileCountX, tileCountY];
+        for (int x = 0; x < tileCountX; x++)
+            for (int y = 0; y < tileCountY; y++)
+                tiles[x,y] = GenerateSingleTile(tileSize, x, y);
+    }
+
+    private GameObject GenerateSingleTile(float tileSize, int x, int y)
+    {
+        GameObject tileObject = new GameObject(string.Format("X:{0}, Y:{1}", x, y));
+        tileObject.transform.parent = transform;
+
+        Mesh mesh = new Mesh();
+        tileObject.AddComponent<MeshFilter>().mesh = mesh;
+        tileObject.AddComponent<MeshRenderer>().material = tileMaterial;
+
+        Vector3[] vertices = new Vector3[4];
+        vertices[0] = new Vector3(x * tileSize, yOffSet, y * tileSize) - bounds;
+        vertices[1] = new Vector3(x * tileSize, yOffSet, (y+1) * tileSize) - bounds;
+        vertices[2] = new Vector3((x+1) * tileSize, yOffSet, y * tileSize) - bounds;
+        vertices[3] = new Vector3((x+1) * tileSize, yOffSet, (y+1) * tileSize) - bounds;
+
+        int[] tris = new int[] { 0, 1, 2, 1, 3, 2 };
+
+        mesh.vertices = vertices;
+        mesh.triangles = tris;
+        mesh.RecalculateNormals();
+
+        tileObject.layer = LayerMask.NameToLayer("Tile");
+        tileObject.AddComponent<BoxCollider>();
+
+        return tileObject;
+    }
+
+    // Spawning of the cards
+    private void SpawnAllCards()
+    {
+
+    }
+
+    private Cards SpawnSingleCard(CardPieceType type, int team)
+    {
+        // Instancia o prefab da carta
+        Cards cp = Instantiate(prefabs[(int)type - 1], transform).GetComponent<Cards>();
+
+        // Define o tipo e o time da carta
+        cp.type = type;
+        cp.team = team;
+
+        // Pega o SpriteRenderer
+        SpriteRenderer spriteRenderer = cp.GetComponent<SpriteRenderer>();
+
+        return cp;
+    }
+
+    // Operations
+    private Vector2Int LookupTileIndex(GameObject hitInfo)
+    {
+        for (int x = 0; x < TILE_COUNT_X; x++)
+            for (int y = 0; y < TILE_COUNT_Y; y++)
+                if(tiles[x,y] == hitInfo)
+                    return new Vector2Int(x, y);
+
+        return -Vector2Int.one; // Invalid
+    }
+}
---
+++ Assets/Scripts/Cards/Cards.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public enum CardPieceType
+{
+    None = 0,
+    Rook = 1,
+    Bishop = 2,
+    Queen = 3,
+    King = 4
+}
+
+public class Cards : MonoBehaviour
+{
+    public int team;
+    public int currentX;
+    public int currentY;
+    public CardPieceType type;
+
+    private Vector3 desiredPosition;
+    private Vector3 desiredScale;
+}
