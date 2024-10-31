da1bb65c799251ad3357c7f676a34f9e892b35e9
Carlos Alberto <carlos.neurozze@gmail.com>
2024-10-31 16:40:03 -0300

    add PROGRAMMABLE OBJECTS in cards

---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    public CardsObjProgram card;
+
+    public Text nameText;
+    public Text descriptionText;
+    public Image ArtImage;
+    public Text manaOrGoldCostText;
+    public Text attackText;
+    public Text shieldText;
+    public Text healthText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText.text = card.cardName;
+        descriptionText.text = card.description;
+        ArtImage.sprite = card.Art;
+        manaOrGoldCostText.text = card.manaOrGoldCost.ToString();
+        attackText.text = card.attack.ToString();
+        shieldText.text = card.shield.ToString();
+        healthText.text = card.health.ToString();
+    }
+}
---
+++ Assets/Scripts/Cards/CardsObjProgram.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class CardsObjProgram : ScriptableObject
+{
+    public string cardName;
+    public string description;
+    public Sprite Art;
+    public int manaOrGoldCost;
+    public int attack;
+    public int shield;
+    public int health;
+
+    public void Print()
+    {
+        Debug.Log(cardName + ": " + description + "The card costs: " + manaOrGoldCost);
+    }
+}
